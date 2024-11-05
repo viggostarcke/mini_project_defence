be5ea94f5e29c3e40db6272a58fcda49df6b5c13
MalteBlackN <mbln@itu.dk>
2024-11-05 19:43:47 +0100

    parametazied get notes by id

--- MySecretNotes/app.py
+++ MySecretNotes/app.py
@@ -82,8 +82,8 @@ def notes():
             noteid = request.form['noteid']
             db = connect_db()
             c = db.cursor()
-            statement = """SELECT * from NOTES where publicID = %s""" %noteid
-            c.execute(statement)
+            statement = """SELECT * from NOTES where publicID = ?"""
+            c.execute(statement, (noteid,))
             result = c.fetchall()
             if(len(result)>0):
                 row = result[0]
