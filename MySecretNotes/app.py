9fd824c7b3cc2a58718dbbcb51dff3746dda36ac
MalteBlackN <mbln@itu.dk>
2024-11-05 19:40:04 +0100

    parameterized post functiong for notes

--- MySecretNotes/app.py
+++ MySecretNotes/app.py
@@ -73,9 +73,9 @@ def notes():
             note = request.form['noteinput']
             db = connect_db()
             c = db.cursor()
-            statement = """INSERT INTO notes(id,assocUser,dateWritten,note,publicID) VALUES(null,%s,'%s','%s',%s);""" %(session['userid'],time.strftime('%Y-%m-%d %H:%M:%S'),note,random.randrange(1000000000, 9999999999))
+            statement = """INSERT INTO notes(id, assocUser, dateWritten, note, publicID) VALUES(null, ?, ?, ?, ?);"""
             print(statement)
-            c.execute(statement)
+            c.execute(statement, (session['userid'], time.strftime('%Y-%m-%d %H:%M:%S'), note, random.randrange(1000000000, 9999999999)))
             db.commit()
             db.close()
         elif request.form['submit_button'] == 'import note':
@@ -87,8 +87,8 @@ def notes():
             result = c.fetchall()
             if(len(result)>0):
                 row = result[0]
-                statement = """INSERT INTO notes(id,assocUser,dateWritten,note,publicID) VALUES(null,%s,'%s','%s',%s);""" %(session['userid'],row[2],row[3],row[4])
-                c.execute(statement)
+                statement = """INSERT INTO notes(id, assocUser, dateWritten, note, publicID) VALUES(null, ?, ?, ?, ?);"""
+                c.execute(statement, (session['userid'], row[2], row[3], row[4]))
             else:
                 importerror="No such note with that ID!"
             db.commit()
